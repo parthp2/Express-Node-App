f41003053883eed2ea91060c4df91307cc2737da
parthp2 <patelparth622@gmail.com>
2018-08-28 18:40:36 -0700

    handlebar partial and helpers

--- server.js
+++ server.js
@@ -6,9 +6,18 @@ var app = express();
 
 
 
+hbs.registerPartials(__dirname+'/views/partials')
 app.set('view engine','hbs');
 //to use middleware in application here inbuilt express static middleware
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getcurrentYear',()=>{
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIT',(text)=>{
+    return text.toUpperCase()
+})
 
 app.get('/',(req,res)=>{
 
